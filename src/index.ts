2ce84f41c26e688b3b4c7dd1d88aa6b51e5507d8
aezazali1997 <aezazali.pro@gmail.com>
2022-10-22 17:46:09 +0500

    added types and proper prompt

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,45 @@
-import inquirer from "inquirer";                          
+import inquirer, { Answers } from "inquirer";
+
 /* eslint-disable */
 let evaluation = "";
 inquirer
   .prompt([
     {
-      name: "Number",
+      name: "number",
       type: "input",
       message: "Enter first number",
     },
   ])
-  .then((answer1) => {
-    evaluation += String(answer1.Number);
-    inquirer.prompt([
-      {
-        name: "Operation",
-        type: "input",
-        message: "Enter Operation",
-      },
-    ])
-    .then((operationAnswer)=>{
-      evaluation+=operationAnswer.Operation;
-       inquirer.prompt([
-         {
-           name: "Number",
-           type: "input",
-           message: "Enter second number",
-         },
-       ]).then((answer2)=>{
-        evaluation += String(answer2.Number);
-        console.log(eval(evaluation));
-       })
-    })
-    .catch((e)=>{
-      console.log('ERROR: error in operation anser');
-    })
+  .then((answers: Answers) => {
+    evaluation += String(answers.number);
+    inquirer
+      .prompt([
+        {
+          name: "operation",
+          type: "list",
+          message: "Select the operation (+ => 1, - => 2 * => 3, / => 4)",
+          choices: ["+", "-", "*", "/"],
+        },
+      ])
+      .then((operationAnswer: Answers) => {
+        evaluation += operationAnswer.operation;
+        inquirer
+          .prompt([
+            {
+              name: "number",
+              type: "input",
+              message: "Enter second number",
+            },
+          ])
+          .then((answer: Answers) => {
+            evaluation += String(answer.number);
+            console.log("Result = " + eval(evaluation));
+          });
+      })
+      .catch((e) => {
+        console.log("ERROR: error in operation anser");
+      });
   })
   .catch((error) => {
     console.log("Error", error);
   });
-
-
